f60a07448aad5f0c09d4c0a7676c857bc99f0a5f
Ramal <ramal.has2010@gmail.com>
2025-02-09 13:06:51 +0300

    lesson 6 homework finished

---
+++ lesson6-hm/homework.js
@@ -0,0 +1,42 @@
+// ! [2, 4, 6, 8] hər bir elementinin kvadratını hesablayan bir funksiya yaz.
+const numbers1 = [2, 4, 6, 8];
+// *const findTheSquareOfArrayNumbers = (array) => {
+//  * return array.forEach((element) => {
+//   *  Math.sqrt(element);
+//  * });
+// *};
+// *console.log(findTheSquareOfArrayNumbers(numbers1));
+// *console.log(numbers1);
+
+// console.log(numbers1.forEach((number) => number ** 2));
+
+// ! [1, 2, 3, 4, 5, 6] yalnız tək ədədləri seçən bir funksiya yaz.
+// const numbers2 = [1, 2, 3, 4, 5, 6];
+// const chooseTheOddArrayNumbers = (array) => {
+//   return array % 2 == 1;
+// };
+// const oddNumbers = numbers2.filter(chooseTheOddArrayNumbers);
+// console.log(oddNumbers);
+
+// ! [10, 20, 30, 40] bütün elementlərinin cəmini tap.
+// const numbers3 = [10, 20, 30, 40];
+// const sumArrayNumbers = (array) => {
+//   console.log(
+//     array.reduce((previousValue, initialValue) => previousValue + initialValue)
+//   );
+// };
+// sumArrayNumbers(numbers3);
+
+// ! [5, 10, 15] hər bir elementi 2yə vuraraq console.log-da yazdırt.
+// const numbers4 = [5, 10, 15];
+// const hasil2 = numbers4.forEach((element) => element * 2);
+// console.log(hasil2);
+
+// ! [7, 15, 24, 36] müəyyən şərtə uyğun olan ilk elementi tap.
+// const numbers5 = [7, 15, 24, 36];
+// const findTheNumberDividesWith3 = (array) => {
+//   console.log(array.find((number) => number % 3 == 0));
+// };
+// findTheNumberDividesWith3(numbers5);
+
+// ?forEach ancaq undefined qaytarir?
---
+++ lesson6-hm/homework.md
@@ -0,0 +1,11 @@
+# Tapşırıqlar
+
+### 1. [2, 4, 6, 8] hər bir elementinin kvadratını hesablayan bir funksiya yaz.
+
+### 2. [1, 2, 3, 4, 5, 6] yalnız tək ədədləri seçən bir funksiya yaz.
+
+### 3. [10, 20, 30, 40] bütün elementlərinin cəmini tap.
+
+### 4. [5, 10, 15] hər bir elementi 2yə vuraraq console.log-da yazdırt.
+
+### 5. [7, 15, 24, 36] müəyyən şərtə uyğun olan ilk elementi tap.
